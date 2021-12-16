7f6eca407ed7f762bf1b0f3a4ba506bdfaed13b5
lifegpc <g1710431395@gmail.com>
2021-12-16 09:33:16 +0800

    Create a repo for utils

--- cmake/test_strerror_r.c
+++
@@ -1,8 +0,0 @@
-#include <string.h>
-
-int main() {
-    char buf[280];
-    char* s = strerror_r(ENOENT, buf, 280);
-    printf("%s", s);
-    return 0;
-}
--- utils/cmath.c
+++
@@ -1,6 +0,0 @@
-#include "cmath.h"
-
-int GCD(int a, int b) {
-    if (b) while ((a %= b) && (b %= a));
-    return a + b;
-}
