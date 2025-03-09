2fea785d84e79ed87867cd40ee63d9cbeceaece4
lifegpc <root@lifegpc.com>
2025-03-09 19:36:45 +0800

    fix: 支持新版本libavcodec的通道布局API

--- rssbotlib/warp.c
+++ rssbotlib/warp.c
@@ -1,7 +1,14 @@
 #include "warp.h"
+#include "libavcodec/version.h"
+
+#if LIBAVCODEC_VERSION_INT < AV_VERSION_INT(59, 24, 100)
+#define OLD_CHANNEL_LAYOUT 1
+#else
+#define NEW_CHANNEL_LAYOUT 1
+#endif
 
 int get_codecpar_channels(AVCodecParameters* par) {
-    #ifdef FF_API_OLD_CHANNEL_LAYOUT
+    #if NEW_CHANNEL_LAYOUT
         return par->ch_layout.nb_channels;
     #else
         return par->channels;
